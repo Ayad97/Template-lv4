606ae4f47a7e52e6eafaf0f5c9eb85eea8c0b735
Mahmoud Ayad <30412592+Ayad97@users.noreply.github.com>
2019-07-16 19:31:49 +0200

    Add files via upload

---
+++ js/scripte.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('html').niceScroll({
+        cursorcolor:'#2fa943',
+        cursorwidth:4,
+        cursorborder:'1px solid #000',
+        
+    });
+});
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+      $("#myModal").modal();
+    });
+  });
